5dce9eb87cc6ead0c8274485ec4a66eca2b2be53
tsenga2 <50138386+tsenga2@users.noreply.github.com>
2022-05-30 14:42:09 +0900

    Initial files uploaded
    on business cycles

---
+++ bcycle_class.m
@@ -0,0 +1,89 @@
+
+clc
+clear all
+close all
+
+
+f = fred
+startdate = '01/01/1954';
+enddate = '01/01/2019';
+
+d1 = fetch(f,'GCE',startdate,enddate)   
+d2 = fetch(f,'GPDI',startdate,enddate) 
+d3 = fetch(f,'NETEXP',startdate,enddate)
+d4 = fetch(f,'PCEC',startdate,enddate)    
+
+q = d1.Data(:,1);
+
+g = d1.Data(:,2);
+i = d2.Data(:,2);
+nex = d3.Data(:,2);
+c = d4.Data(:,2);
+
+figure
+subplot(2,2,1);
+plot(q, g)
+datetick('x', 'yyyy-qq')
+ylabel('Billions of Dollars')
+xlabel('Time')
+grid on
+
+subplot(2,2,2);
+plot(q, i)
+datetick('x', 'yyyy-qq')
+ylabel('Billions of Dollars')
+xlabel('Time')
+grid on
+
+subplot(2,2,3);
+plot(q, nex)
+datetick('x', 'yyyy-qq')
+ylabel('Billions of Dollars')
+xlabel('Time')
+grid on
+
+subplot(2,2,4);
+plot(q, c)
+datetick('x', 'yyyy-qq')
+ylabel('Billions of Dollars')
+xlabel('Time')
+grid on;
+
+
+
+%%
+realGDP = fetch(f,'GDPCA',startdate,enddate)      %Real Gross Domestic Product
+year = realGDP.Data(:,1);
+y = realGDP.Data(:,2);
+
+
+figure
+plot(year, log(y))
+datetick('x', 'yyyy')
+ylabel('Log of real GDP (billions of chained 2012 dollars)')
+xlabel('')
+grid on
+
+[trend, cycle] = hpfilter(log(y),6.25);
+
+% compute sd(y) (from detrended series)
+ysd = std(cycle)*100;
+
+disp(['Percent standard deviation of detrended log real GDP: ', num2str(ysd),'.']); disp(' ')
+
+figure
+subplot(2,1,1);
+plot(year, trend,'b')
+datetick('x', 'yyyy')
+xlabel('Time')
+title('Trend components')
+grid on
+
+subplot(2,1,2);
+plot(year, cycle,'r')
+datetick('x', 'yyyy')
+xlabel('Time')
+title('Cyclical components')
+grid on;
+
+
---
+++ f_hpfilter.m
@@ -0,0 +1,50 @@
+
+clc
+clear all
+close all
+
+% load the data
+startdate = '01/01/1954';
+enddate = '01/01/2019';
+f = fred
+Y = fetch(f,'GDPC1',startdate,enddate)
+y = log(Y.Data(:,2));
+q = Y.Data(:,1);
+
+T = size(y,1);
+
+% Hodrick-Prescott filter
+lam = 1600;
+A = zeros(T,T);
+
+% unusual rows
+A(1,1)= lam+1; A(1,2)= -2*lam; A(1,3)= lam;
+A(2,1)= -2*lam; A(2,2)= 5*lam+1; A(2,3)= -4*lam; A(2,4)= lam;
+
+A(T-1,T)= -2*lam; A(T-1,T-1)= 5*lam+1; A(T-1,T-2)= -4*lam; A(T-1,T-3)= lam;
+A(T,T)= lam+1; A(T,T-1)= -2*lam; A(T,T-2)= lam;
+
+% generic rows
+for i=3:T-2
+    A(i,i-2) = lam; A(i,i-1) = -4*lam; A(i,i) = 6*lam+1;
+    A(i,i+1) = -4*lam; A(i,i+2) = lam;
+end
+
+tauGDP = A\y;
+
+% detrended GDP
+ytilde = y-tauGDP;
+
+% plot detrended GDP
+dates = 1954:1/4:2019.1/4; zerovec = zeros(size(y));
+figure
+title('Detrended log(real GDP) 1954Q1-2019Q1'); hold on
+plot(q, ytilde,'b', q, zerovec,'r')
+datetick('x', 'yyyy-qq')
+
+% compute sd(y), sd(c), rho(y), rho(c), corr(y,c) (from detrended series)
+ysd = std(ytilde)*100;
+yrho = corrcoef(ytilde(2:T),ytilde(1:T-1)); yrho = yrho(1,2);
+
+disp(['Percent standard deviation of detrended log real GDP: ', num2str(ysd),'.']); disp(' ')
+
